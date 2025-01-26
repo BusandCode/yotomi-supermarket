63d837a08c5128ba3213e1a1d7af8a3f654eec43
Adetokunbo Andrew <Adetokunboandrew2@gmail.com>
2025-01-27 00:22:27 +0100

    egusi-section

--- app/food/egusi/page.js
+++ app/food/egusi/page.js
@@ -10,7 +10,7 @@ const page = () => {
   return (
     <main className='max-w-[450px]  mx-auto flex flex-col  gap-5 h-full'>
         <div className='flex justify-between items-center py-5 px-3'>
-            <h1 className='text-[25px] font-bold'><i>Yoto<span className='text-[#60D669]'>mi</span></i></h1>
+            <h1 className='text-[30px] font-bold'><i>Yoto<span className='text-[#60D669]'>mi</span></i></h1>
             <div className='flex items-center gap-5 text-[#60D669]'>
               <HiOutlineShoppingCart className='cursor-pointer' size={25}/>
               <FaUser  className='bg-[#60D669] cursor-pointer bg-opacity-40 w-[36px] h-[36px] p-2 rounded-full' size={25}/>
@@ -24,10 +24,10 @@ const page = () => {
           <div className='flex items-center justify-between py-5 px-3'>
             {/* Description */}
             <div className='flex flex-col items-start gap-5 '>
-              <h1 className='text-[#000000] font-semibold text-[15px]'>Egusi Soup</h1>
-              <p className='text-[11px] font-normal'>1 Pack + 2 Meat Pieces</p>
+              <h1 className='text-[#000000] font-semibold text-[17px]'>Egusi Soup</h1>
+              <p className='text-[13px] font-normal'>1 Pack + 2 Meat Pieces</p>
               <div className='flex items-center gap-3'>
-                <h2 className='text-[#1FAF38] text-[15px] font-normal'>N2000</h2>
+                <h2 className='text-[#1FAF38] text-[17px] font-normal'>N2000</h2>
                 <button className='bg-[#60D669] text-[#FFFFFF] px-[8px] text-[10px] py-[4px] rounded-[4px]'>Add to cart</button>
               </div>
             </div>
@@ -40,10 +40,10 @@ const page = () => {
           <div className='flex items-center justify-between py-5 px-3'>
             {/* Description */}
             <div className='flex flex-col items-start gap-5 '>
-              <h1 className='text-[#000000] font-semibold text-[15px]'>Egusi Soup</h1>
-              <p className='text-[11px] font-normal'>2 Packs + 2 Meat</p>
+              <h1 className='text-[#000000] font-semibold text-[17px]'>Egusi Soup</h1>
+              <p className='text-[13px] font-normal'>2 Packs + 2 Meat</p>
               <div className='flex items-center gap-3'>
-                <h2 className='text-[#1FAF38] text-[15px] font-normal'>N7000</h2>
+                <h2 className='text-[#1FAF38] text-[17px] font-normal'>N7000</h2>
                 <button className='bg-[#60D669] text-[#FFFFFF] px-[8px] text-[10px] py-[4px] rounded-[4px]'>Add to cart</button>
               </div>
             </div>
@@ -56,10 +56,10 @@ const page = () => {
           <div className='flex items-center justify-between py-5 px-3'>
             {/* Description */}
             <div className='flex flex-col items-start gap-5 '>
-              <h1 className='text-[#000000] font-semibold text-[15px]'>Egusi Soup</h1>
-              <p className='text-[11px] font-normal'>3 Packs + 5 Meats + 1 Table Water</p>
+              <h1 className='text-[#000000] font-semibold text-[17px]'>Egusi Soup</h1>
+              <p className='text-[13px] font-normal'>3 Packs + 5 Meats + 1 Table Water</p>
               <div className='flex items-center gap-3'>
-                <h2 className='text-[#1FAF38] text-[15px] font-normal'>N10,000</h2>
+                <h2 className='text-[#1FAF38] text-[17px] font-normal'>N10,000</h2>
                 <button className='bg-[#60D669] text-[#FFFFFF] px-[8px] text-[10px] py-[4px] rounded-[4px]'>Add to cart</button>
               </div>
             </div>
@@ -79,7 +79,7 @@ const page = () => {
           </section>
 
           {/* Footer */}
-          <div className='text-[11px] bg-[#60D669] bg-opacity-25 max-w-[450px] w-full h-[136px]  flex justify-between items-center  mt-12  rounded-tl-[30px] rounded-tr-[30px] p-5 text-[#062C0C]'>
+          <div className='text-[13px] bg-[#60D669] bg-opacity-25 max-w-[450px] w-full h-[136px]  flex justify-between items-center  mt-12  rounded-tl-[30px] rounded-tr-[30px] p-5 text-[#062C0C]'>
         <p>&copy; 2025, Yotomi</p>
         <div className='flex gap-5 items-center'>
           <a href="">Contact Us</a>
--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ const page = () => {
       <section className=' py-5 px-3 bg-[#DAF6DC] flex flex-col gap-10 rounded-br-[160px] rounded-tl-[50px] '>
       {/* Header */}
     <div className='flex justify-between items-center'>
-      <h1 className='text-[25px] font-bold'><i>Yoto<span className='text-[#60D669]'>mi</span></i></h1>
+      <h1 className='text-[30px] font-bold'><i>Yoto<span className='text-[#60D669]'>mi</span></i></h1>
       <div className='flex items-center gap-5 text-[#60D669]'>
         <HiOutlineShoppingCart className='cursor-pointer' size={25}/>
         <FaUser  className='bg-[#60D669] cursor-pointer bg-opacity-40 w-[36px] h-[36px] p-2 rounded-full' size={25}/>
@@ -27,26 +27,26 @@ const page = () => {
       {/* Items-List */}
     <div className='flex flex-col items-center gap-3 p-3'>
       <Link href="/food/egusi" className='flex items-center justify-between cursor-pointer bg-[#DAF6DC] rounded-[8px] h-[60px] w-full p-8 hover:bg-[#60D669]'>
-        <button className='text-[15px] font-medium text-[#062C0C]'>Egusi</button>
+        <button className='text-[17px] font-medium text-[#062C0C]'>Egusi</button>
         <MdNavigateNext className='bg-[#E2F8E3] text-[#14AE5C] w-[24px] h-[24px] rounded-full' size={200}/>
       </Link>
       <Link href="/food/efo" className='flex items-center justify-between cursor-pointer bg-[#DAF6DC] rounded-[8px] h-[60px] w-full p-8 hover:bg-[#60D669]'>
-        <button className='text-[15px] font-medium text-[#062C0C]'>Efo</button>
+        <button className='text-[17px] font-medium text-[#062C0C]'>Efo</button>
         <MdNavigateNext className='bg-[#E2F8E3] text-[#14AE5C] w-[24px] h-[24px] rounded-full' size={200}/>
       </Link>
       <div className='flex items-center justify-between cursor-pointer bg-[#DAF6DC] rounded-[8px] h-[60px] w-full p-8 hover:bg-[#60D669]'>
-        <button className='text-[15px] font-medium text-[#062C0C]'>Red Stew</button>
+        <button className='text-[17px] font-medium text-[#062C0C]'>Red Stew</button>
         <MdNavigateNext className='bg-[#E2F8E3] text-[#14AE5C] w-[24px] h-[24px] rounded-full' size={200}/>
       </div>
       <div className='flex items-center justify-between cursor-pointer bg-[#DAF6DC] rounded-[8px] h-[60px] w-full p-8 hover:bg-[#60D669] '>
-        <button className='text-[15px] font-medium text-[#062C0C]'>Gbegiri & Ewedu</button>
+        <button className='text-[17px] font-medium text-[#062C0C]'>Gbegiri & Ewedu</button>
         <MdNavigateNext className='bg-[#E2F8E3] text-[#14AE5C] w-[24px] h-[24px] rounded-full' size={200}/>
       </div>
     </div>
 
     {/* Footer */}
     {/* Footer */}
-    <div className='text-[11px] bg-[#60D669] bg-opacity-25 max-w-[450px] w-full h-[136px]  flex justify-between items-center  mt-12  rounded-tl-[30px] rounded-tr-[30px] p-5 text-[#062C0C]'>
+    <div className='text-[13px] bg-[#60D669] bg-opacity-25 max-w-[450px] w-full h-[136px]  flex justify-between items-center  mt-12  rounded-tl-[30px] rounded-tr-[30px] p-5 text-[#062C0C]'>
         <p>&copy; 2025, Yotomi</p>
         <div className='flex gap-5 items-center'>
           <a href="">Contact Us</a>
