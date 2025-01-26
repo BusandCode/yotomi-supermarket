8855fe8594e36583b1df91ad7a73fd816712e7d5
Adetokunbo Andrew <Adetokunboandrew2@gmail.com>
2025-01-27 00:40:58 +0100

    egusi-section

--- app/page.js
+++ app/page.js
@@ -45,14 +45,14 @@ const page = () => {
     </div>
 
     {/* Footer */}
-    {/* Footer */}
-    <div className='text-[13px] bg-[#60D669] bg-opacity-25 max-w-[450px] w-full h-[136px]  flex justify-between items-center  mt-12  rounded-tl-[30px] rounded-tr-[30px] p-5 text-[#062C0C]'>
-        <p>&copy; 2025, Yotomi</p>
-        <div className='flex gap-5 items-center'>
-          <a href="">Contact Us</a>
-          <a href="">About Us</a>
+              {/* Footer */}
+              <div className='text-[13px] bg-[#60D669] bg-opacity-25 max-w-[450px] w-full h-[136px]  flex justify-between items-center  mt-12  rounded-tl-[30px] rounded-tr-[30px] p-5 text-[#062C0C]'>
+            <p  className='flex items-center justify-center gap-1'><FaRegCopyright size={17}/> 2025, Yotomi</p>
+            <div className='flex gap-5 items-center'>
+              <a href="">Contact Us</a>
+              <a href="">About Us</a>
+            </div>
         </div>
-    </div>
     </main>
   )
 }
